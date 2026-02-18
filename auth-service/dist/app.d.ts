f98483e2b1ed499eae9983d06e3b615974c83df0
josefo1212 <jffcastillo0@gmail.com>
2026-02-18 13:56:10 -0400

    Feat: Se creo el auth-service y se establecio la conexion a la base de datos

---
+++ auth-service/dist/app.d.ts
@@ -0,0 +1,5 @@
+import { Pool } from 'pg';
+declare const app: import("express-serve-static-core").Express;
+export declare const db: Pool;
+export default app;
+//# sourceMappingURL=app.d.ts.map
---
+++ auth-service/src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Configuración de la base de datos
+export const db = new Pool({
+	host: process.env.DB_HOST,
+	port: Number(process.env.DB_PORT),
+	database: process.env.DB_NAME,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+});
+
+db.connect()
+	.then(() => console.log('Connected to PostgreSQL'))
+	.catch((err) => console.error('Database connection error:', err));
+
+
+
+export default app;
