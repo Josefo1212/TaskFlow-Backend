e13af5beebe20f553752b9ae3e982cd9013d2e99
josefo1212 <jffcastillo0@gmail.com>
2026-03-24 22:35:20 -0400

    Feat:Se agrego en los contenedores la dependencia de pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,6 +17,42 @@ module.exports = {
         PORT: 3003,
         NODE_ENV: "development"
       }
+    },
+    {
+      name: "user-service",
+      script: "dist/index.js",
+      cwd: "./user-service",
+      env: {
+        PORT: 3002,
+        NODE_ENV: "development"
+      }
+    },
+    {
+      name: "task-service",
+      script: "dist/index.js",
+      cwd: "./task-service",
+      env: {
+        PORT: 3004,
+        NODE_ENV: "development"
+      }
+    },
+    {
+      name: "tag-service",
+      script: "dist/index.js",
+      cwd: "./tag-service",
+      env: {
+        PORT: 3005,
+        NODE_ENV: "development"
+      }
+    },
+    {
+      name: "project-service",
+      script: "dist/index.js",
+      cwd: "./project-service",
+      env: {
+        PORT: 3006,
+        NODE_ENV: "development"
+      }
     }
   ]
 };
